a45d0032fb31711a071abadfe372977bbca7a5a5
Lanskoy <lanskoyvkad@gmail.com>
2026-06-18 04:37:44 +0300

    Prepare database connection for production

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,30 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
-});
+const isProduction = process.env.NODE_ENV === 'production';
+
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: isProduction
+          ? {
+              rejectUnauthorized: false
+            }
+          : false
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        ssl: isProduction
+          ? {
+              rejectUnauthorized: false
+            }
+          : false
+      }
+);
 
 module.exports = pool;
